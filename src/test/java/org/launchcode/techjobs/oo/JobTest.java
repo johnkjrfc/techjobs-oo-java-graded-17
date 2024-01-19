771df92072a73376da1a0c784d10b97b42c7394f
johnkjrfc <johnkjrfc@gmail.com>
2024-01-19 10:25:38 -0600

    task 4 complete

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,44 @@
 package org.launchcode.techjobs.oo;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class JobTest {
     //TODO: Create your unit tests here
+    @Test
+    public void testSettingJobId() {
+        Job test1 = new Job();
+        Job test2 = new Job();
+        System.out.println(test1.getId() + test2.getId());
+        assertNotEquals(test1.getId(), test2.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job test1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"),
+                new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        //test that constructor assigns both class and value for each field
+        assertNotNull("name is not null", test1.getName());
+        assertTrue("employer is instance of Employer", test1.getEmployer() instanceof Employer);
+        assertTrue("location is instance of Location", test1.getLocation() instanceof Location);
+        assertTrue("positionType is instance of PositionType", test1.getPositionType() instanceof PositionType);
+        assertTrue("coreCompetency is an instance of CoreCompetency", test1.getCoreCompetency() instanceof CoreCompetency);
+
+        assertEquals("employer value is expected value", "ACME", test1.getEmployer().toString());
+        assertEquals("location value is expected value", "Desert", test1.getLocation().toString());
+        assertEquals("positionType value is expected value", "Quality control", test1.getPositionType().toString());
+        assertEquals("coreCompetency value is expected value", "Persistence", test1.getCoreCompetency().toString());
+        assertEquals("name value is expected value", "Product tester", test1.getName());
+    }
+    @Test
+    public void testJobsForEquality () {
+        Job test1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"),
+                new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job test2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"),
+                new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        boolean result = test1.equals(test2);
+
+        assertFalse(result);
+    }
 }
